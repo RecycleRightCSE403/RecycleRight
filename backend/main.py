805dfa7b9e01f8099ca25b1eeddfaf8a1056a435
Aaron Jenson <aaronajenson@gmail.com>
2024-02-08 14:21:28 -0800

    Added basic api structure

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/classify_image/")
+async def classify_image(file: UploadFile):
+    # classify image
+    return {"filename": file.filename}
+
+
+@app.get("/classify_text/")
+async def classify_test(text: str):
+    return {"result": text}
+
+
+@app.post("/report_image/")
+async def report_image(file: UploadFile, text: str):
+    return {"filename": file}
