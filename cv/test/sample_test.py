84c864485bba155d86bc90119591ac22ed2ee099
vicjou <vicjou8@gmail.com>
2024-02-11 23:16:35 +0000

    working on testing for cv

---
+++ cv/test/sample_test.py
@@ -0,0 +1,8 @@
+'''
+Type python -m pytest in terminal to run pytest files.
+Create functions with a name starting with 
+test_ as per standard pytest conventions.
+'''
+
+def test_api():
+    return 1
