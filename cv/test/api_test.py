7aff5e4c1910b0a56603da0100e04aae18c08c65
vicjou <vicjou8@gmail.com>
2024-02-12 18:02:30 +0000

    adding tests for cv

---
+++ cv/test/api_test.py
@@ -0,0 +1,10 @@
+import classify
+
+'''
+Type python -m pytest in terminal to run pytest files.
+Create functions with a name starting with 
+test_ as per standard pytest conventions.
+'''
+
+def test_post():
+    assert True
