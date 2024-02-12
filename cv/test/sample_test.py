38fb1ca7b0d78c37e6dd14010ae6be81d49951e1
vicjou <vicjou8@gmail.com>
2024-02-12 00:04:09 +0000

    Revert "working on testing for cv"
    This reverts commit 84c864485bba155d86bc90119591ac22ed2ee099.

--- cv/test/sample_test.py
+++
@@ -1,8 +0,0 @@
-'''
-Type python -m pytest in terminal to run pytest files.
-Create functions with a name starting with 
-test_ as per standard pytest conventions.
-'''
-
-def test_api():
-    return 1
