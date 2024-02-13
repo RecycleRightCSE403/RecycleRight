e83fc6349cbd49bb803410f6c12fba9ee2a20bf8
vicjou <vicjou8@gmail.com>
2024-02-13 22:33:06 +0000

    integration with cv model

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,14 @@
-
 from fastapi import FastAPI, File, UploadFile
 import logging
 import os
 from starlette.responses import FileResponse
 from ml.llm import classify_item
+from cv.classify import classify_image, get_highest_predictions
 
 app = FastAPI()
 
 # Directory to store images
-UPLOAD_DIRECTORY = "../cv/images"
+UPLOAD_DIRECTORY = "images"
 
 @app.post("/classify_image/")
 async def classify_image(file: UploadFile = File(...)):
@@ -16,8 +16,12 @@ async def classify_image(file: UploadFile = File(...)):
     with open(file_location, "wb+") as file_object:
         file_object.write(file.file.read())
     logging.info(f"File '{file.filename}' saved at '{file_location}'")
-    # item = call cv code, and store a single string item in here
-    classification_result = "" # delete empty string and replace with: classify_item(item) 
+    json_response = classify_image(file.filename)
+    classes = get_highest_predictions(json_reponse)
+    item = ''
+    if len(classes != 0):
+        item = classes[0]
+    classification_result = classify_item(item) 
     # logging.info(f"LLM classified object: {item}")
     return {"filename": file.filename, "classification": classification_result}
 
