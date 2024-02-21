e05ecfdd4484c6c0af90bf06f0a696601dc18a20
Aaron Jenson <aaronajenson@gmail.com>
2024-02-21 14:06:14 -0800

    Added CORS and deploy script

---
+++ backend/deploy_ec2.sh
@@ -0,0 +1,34 @@
+# This script is not meant to be run locally. It should be run on an AWS EC2
+# instance, where it will clone the tag set as DEPLOY_TAG env var and set up
+# whatever is needed to start the backend server
+
+# run it on a remote server with this command structure:
+# ssh ubuntu@<aws-hostname> DEPLOY_TAG="..." ROBOFLOW_API_KEY="..." ... 'bash -s' < deploy_ec2.sh
+
+# env vars:
+# DEPLOY_TAG = branch or tag name to deploy
+# ROBOFLOW_API_KEY
+# GEMINI_API_KEY
+
+# assumes python3, nginx are installed and configured
+
+# remove old version
+rm -r RecycleRight
+
+# clone new version
+git clone --depth 1 --branch $DEPLOY_TAG https://github.com/RecycleRightCSE403/RecycleRight.git
+
+cd RecycleRight/backend/
+python3 -m venv venv
+source venv/bin/activate
+
+pip3 install -r requirements.txt
+
+echo ROBOFLOW_API_KEY=$ROBOFLOW_API_KEY > .env
+echo GEMINI_API_KEY=$GEMINI_API_KEY >> .env
+
+pip3 install gunicorn
+
+sudo systemctl daemon-reload
+sudo systemctl restart gunicorn
+sudo systemctl restart nginx
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import logging
 import os
 from starlette.responses import FileResponse
@@ -7,6 +8,15 @@ from cv.classify import get_highest_predictions
 import cv.classify
 
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+ CORSMiddleware,
+ allow_origins=origins,
+ allow_credentials=True,
+ allow_methods=["*"],
+ allow_headers=["*"],
+)
 
 # Directory to store images
 UPLOAD_DIRECTORY = "images"
