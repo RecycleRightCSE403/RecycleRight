990bb4027eddd8210b072e81caee78bf33149070
Aaron Jenson <aaronajenson@gmail.com>
2024-02-21 14:41:26 -0800

    Added staging deploy action

--- backend/deploy_ec2.sh
+++ backend/deploy_ec2.sh
@@ -1,9 +1,9 @@
 # This script is not meant to be run locally. It should be run on an AWS EC2
-# instance, where it will clone the tag set as DEPLOY_TAG env var and set up
+# instance, where it will clone the commit set as DEPLOY_SHA env var and set up
 # whatever is needed to start the backend server
 
 # run it on a remote server with this command structure:
-# ssh ubuntu@<aws-hostname> DEPLOY_TAG="..." ROBOFLOW_API_KEY="..." ... 'bash -s' < deploy_ec2.sh
+# ssh ubuntu@<aws-hostname> DEPLOY_SHA="..." ROBOFLOW_API_KEY="..." ... 'bash -s' < deploy_ec2.sh
 
 # env vars:
 # DEPLOY_TAG = branch or tag name to deploy
@@ -16,12 +16,14 @@
 rm -r RecycleRight
 
 # clone new version
-git clone --depth 1 --branch $DEPLOY_TAG https://github.com/RecycleRightCSE403/RecycleRight.git
+git clone https://github.com/RecycleRightCSE403/RecycleRight.git
+cd RecycleRight/
+git reset --hard $DEPLOY_SHA
 
-cd RecycleRight/backend/
+# create venv
+cd backend/
 python3 -m venv venv
 source venv/bin/activate
-
 pip3 install -r requirements.txt
 
 echo ROBOFLOW_API_KEY=$ROBOFLOW_API_KEY > .env
