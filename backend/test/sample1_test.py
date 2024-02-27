89569d6e8217fc15991843d26e0ba017b7744d1e
sulaimanshahbain <34633390+sulaimanshahbain@users.noreply.github.com>
2024-02-27 01:47:43 -0800

    Temporary sample1_test.py

--- backend/test/sample1_test.py
+++ backend/test/sample1_test.py
@@ -1,18 +1,1 @@
-from fastapi import FastAPI
-from fastapi.testclient import TestClient
-from main import app
 
-'''
-Type python -m pytest in terminal to run pytest files.
-Create functions with a name starting with 
-test_ as per standard pytest conventions.
-'''
-
-
-client = TestClient(app)
-
-def test_classify_text():
-    test_text = "Hello, World"
-    response = client.get(f"/classify_text/?text={test_text}")
-    # assert response.json() == {"result": test_text}
- 
