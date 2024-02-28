38477a09ac2274b012afd5856438c9ff4dac27a4
Aaron Jenson <aaronajenson@gmail.com>
2024-02-27 20:15:31 -0800

    Added default server url (emulator)

--- frontend/recycleright/lib/config.dart
+++ frontend/recycleright/lib/config.dart
@@ -1,5 +1,4 @@
 String getServerBaseUrl() {
-  // return "http://10.0.2.2:8000:";
-  // return "http://192.168.0.102:8000";
-  return const String.fromEnvironment("SERVER_BASE_URL");
+  return const String.fromEnvironment("SERVER_BASE_URL",
+      defaultValue: "http://10.0.2.2:8000");
 }
