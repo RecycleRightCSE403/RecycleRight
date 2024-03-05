e64b0ef371bc2dd4cf44c8e66b05d96fc243deee
vicjou <vicjou8@gmail.com>
2024-03-05 21:31:29 +0000

    removed unused test files

--- backend/test/make_requests.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def make_request(api_key):
-    # Tells request to go and get information from that url
-    base_url = "https://api.nasa.gov/neo/rest/v1/feed"
-    response = requests.get(f'{base_url}?api_key={api_key}')
-    return response
