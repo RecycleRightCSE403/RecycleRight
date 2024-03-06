a45e5bbd962fc81b30432f7bb63979c6c8cb0e65
jtawfik02 <153245824+jtawfik02@users.noreply.github.com>
2024-03-06 01:32:26 -0600

    Documentation frontend (#39)
    * Added about page and updated home page and theme
    
    * Added Documentation to FrontEnd
    
    * linting
    
    * Update results.dart to include showing item
    
    * Update text_classification_result_screen.dart to include item in classification result
    
    ---------
    
    Co-authored-by: JuliaT <jtawfik02@uw.edu>
    Co-authored-by: Aaron Jenson <aaronajenson@gmail.com>
    Co-authored-by: sulaimanshahbain <34633390+sulaimanshahbain@users.noreply.github.com>

--- frontend/recycleright/lib/classification_result_card.dart
+++ frontend/recycleright/lib/classification_result_card.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
 
+// A custom widget that displays the classification result of an item.
+//
+// This widget presents a card with an icon and colors corresponding to the classification
+// category (e.g., recycle, compost, other, garbage, or error), along with advice on how
+// to properly dispose of the item. It's designed to provide a visually appealing and
+// informative summary for the user.
 class ClassificationResultCard extends StatelessWidget {
+  // The category of the item as determined by the classification model.
+  //
+  // This value is used to determine the icon and color to display on the card.
   final String category;
+  // A widget containing advice or instructions related to the item's disposal
+  // based on its category.
   final Widget adviceWidget;
 
+
+  // Constructs a `ClassificationResultCard` widget.
+  //
+  // Requires [category] and [advice] parameters to display classification information.
+  // The [crossAxisAlignment] parameter is also required for layout purposes, though
+  // its usage has been deprecated in favor of internal layout decisions.
   const ClassificationResultCard({
     Key? key,
     required this.category,
     required this.adviceWidget,
   }) : super(key: key);
 
+  // Determines the icon and color associated with the specified category.
+  //
+  // The function maps category names to their corresponding icons and colors.
+  // Categories include 'recycle', 'compost', 'other', 'garbage', and 'error',
+  // with a default fallback for unrecognized categories.
+  //
+  // - Parameters:
+  //   - category: The category name to get the icon and color for.
+  // - Returns: A map containing the 'icon' as an [IconData] and 'color' as a [Color].
   Map<String, dynamic> getIconAndColorForCategory(String category) {
     switch (category.toLowerCase()) {
       case 'recycle':
@@ -29,12 +55,14 @@ class ClassificationResultCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Retrieve icon and color based on the category of the item.
     final categoryData = getIconAndColorForCategory(category);
     final IconData categoryIcon = categoryData['icon'];
     final Color color = categoryData['color'];
 
+    // The card layout includes the icon, category name, and advice text.
     return Card(
-      elevation: 8,
+      elevation: 8, // Adds a subtle shadow for depth effect.
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       child: Container(
         decoration: BoxDecoration(
@@ -42,16 +70,16 @@ class ClassificationResultCard extends StatelessWidget {
           borderRadius: BorderRadius.circular(20),
         ),
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16.0), // Ensures content is nicely spaced within the card.
           child: Column(
-            mainAxisSize: MainAxisSize.min,
+            mainAxisSize: MainAxisSize.min, // Minimizes the column's height to fit its content.
             children: [
               Icon(
                 categoryIcon,
                 size: 60,
                 color: color,
               ),
-              const SizedBox(height: 12),
+              const SizedBox(height: 12), // Spacing between the icon and category name.
               Text(
                 category,
                 style: TextStyle(
@@ -60,7 +88,7 @@ class ClassificationResultCard extends StatelessWidget {
                   color: color,
                 ),
               ),
-              const SizedBox(height: 10),
+              const SizedBox(height: 10), // Spacing between the category name and advice text.
               adviceWidget,
             ],
           ),
--- frontend/recycleright/lib/text_classification_result_screen.dart
+++ frontend/recycleright/lib/text_classification_result_screen.dart
@@ -5,13 +5,23 @@ import 'dart:convert';
 import 'classification_result_card.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+// `TextClassificationResultScreen` is a StatelessWidget that presents the user's text input
+// and its corresponding classification result.
+//
+// This screen is used to display the outcome of a classification process, showing both the
+// original text input by the user and the classification result returned by the system.
 class TextClassificationResultScreen extends StatefulWidget {
+  // The text input provided by the user for classification.
   final String userInput;
 
   const TextClassificationResultScreen(
       {Key? key, required this.userInput, required String classificationResult})
       : super(key: key);
 
+  // Constructs a `TextClassificationResultScreen` widget.
+  //
+  // Requires [userInput] to display the text that was classified and [classificationResult]
+  // to display the outcome of the classification process.
   @override
   State<TextClassificationResultScreen> createState() =>
       _TextClassificationResultScreenState();
@@ -36,7 +46,7 @@ class _TextClassificationResultScreenState
 
     if (response.statusCode == 200) {
       var result = jsonDecode(response.body);
-      print("Decoded JSON response: $result");
+      // print("Decoded JSON response: $result");
 
       String classificationKeyword =
           result['classification']['classification'].toString().trim();
@@ -47,8 +57,8 @@ class _TextClassificationResultScreenState
               classificationKeyword == 'special') &&
           result['classification'].containsKey('locations')) {
         String prefixText = classificationKeyword == 'donate'
-            ? "Google Link For Locations to Donate:"
-            : "Google Link For Locations to Consider:";
+            ? "Find Donation Centers Here:"
+            : "Discover Special Disposal Sites Here:";
 
         var links = [
           "https://www.google.com/search?q=where+to+drop+off+$itemName+in+Seattle"
@@ -69,17 +79,18 @@ class _TextClassificationResultScreenState
                 ))
             .toList());
       } else {
-        String adviceText;
-        if (classificationKeyword == 'recycle' &&
-            result['classification'].containsKey('specifications')) {
-          adviceText = result['classification']['specifications'].join('\n');
-        } else if (classificationKeyword == 'garbage') {
-          adviceText = "Please dispose of item in garbage.";
-        } else if (classificationKeyword == 'compost') {
-          adviceText = "Please compost item.";
-        } else {
-          adviceText = "";
-        }
+        String adviceText = "Check item specifics for proper disposal.";
+          if (classificationKeyword == 'recycle' &&
+              result['classification'].containsKey('specifications')) {
+            String specificationsText =
+                result['classification']['specifications'].join('\n');
+            adviceText =
+                "$specificationsText\n\nPlease recycle $itemName responsibly.";
+          } else if (classificationKeyword == 'garbage') {
+            adviceText = "Please dispose of $itemName in the garbage.";
+          } else if (classificationKeyword == 'compost') {
+            adviceText = "Please compost $itemName.";
+          }
         extraInfoWidgets.add(Text(adviceText,
             style: const TextStyle(fontWeight: FontWeight.bold)));
       }
@@ -96,7 +107,7 @@ class _TextClassificationResultScreenState
       setState(() {
         classificationResult = "Error";
         adviceWidget = const Text(
-            "LLM Server may be down. Please check your internet and try again.",
+            "LLM server may be down. Please check your internet and try again.",
             style: TextStyle(fontWeight: FontWeight.bold));
       });
     }
@@ -110,6 +121,7 @@ class _TextClassificationResultScreenState
 
   @override
   Widget build(BuildContext context) {
+    // Scaffold provides the high-level structure for the results screen.
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -118,7 +130,7 @@ class _TextClassificationResultScreenState
             fontSize: 35,
             fontWeight: FontWeight.bold,
           ),
-        ),        
+        ),
         centerTitle: true,
       ),
       body: Center(
